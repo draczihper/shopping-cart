f83f068f95ae721aaafc28800e079fcbeaa470b8
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-13 23:11:57 +0300

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './home'
 
 function App() {
 
   return (
     <>
-
+      <Home />
+      <Shop />
+      <Cart />
     </>
   )
 }
