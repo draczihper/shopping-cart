76325f63e0f59b18d18d48eb97116e1a217c8ade
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-15 00:01:33 +0300

    Change file name

---
+++ src/Cart.jsx
@@ -0,0 +1,10 @@
+const Cart = () => {
+
+    return (
+        <>
+        <h3>This is a cart</h3>
+        </>
+    )
+}
+
+export default Cart;
---
+++ src/Shop.jsx
@@ -0,0 +1,12 @@
+const Shop = () => {
+
+
+    return (
+        <>
+        <h2>This is a shop</h2>
+        </>
+    )
+}
+
+
+export default Shop;
