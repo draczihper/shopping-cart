b1681094fb23550bfc91a9e8192d5eaa0d70262b
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-15 19:04:55 +0300

    Add routes to our app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./Home.jsx";
+import Cart from "./Cart.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "home",
+    element: <Home />
+  },
+  {
+    path: "shop",
+    element: <Shop />
+  },
+  {
+    path: "cart",
+    element: <Cart />
+  },
+])
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
