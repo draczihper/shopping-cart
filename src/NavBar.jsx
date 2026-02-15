584aff3f626681c9261af03f8655706bc37a2ffe
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-15 19:17:34 +0300

    Add navbar component

---
+++ src/NavBar.jsx
@@ -0,0 +1,21 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="Home">HOME</a>
+          </li>
+          <li>
+            <a href="Shop">SHOP</a>
+          </li>
+          <li>
+            <a href="Cart">CART</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
