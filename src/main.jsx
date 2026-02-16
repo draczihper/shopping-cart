aa00e86b662b5bb8af4adf91717438969c569ec6
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-16 23:17:47 +0300

    Add route config

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider} from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import Home from "./Home.jsx";
 import Cart from "./Cart.jsx";
+import Shop from "./Shop.jsx";
+import NavBar from "./NavBar.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
   },
   {
-    path: "home",
-    element: <Home />
-  },
-  {
-    path: "shop",
-    element: <Shop />
-  },
-  {
-    path: "cart",
-    element: <Cart />
-  },
+    path: "NavBar",
+    element: <NavBar />
+  }
 ])
 
 createRoot(document.getElementById("root")).render(
