b18ba4781bd090319b3c5451c4bcfb61d8b486a7
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-20 23:18:53 +0300

    Add new module

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider} from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import NavBar from "./NavBar.jsx";
-import Home from "./Home.jsx";
-import Cart from "./Cart.jsx";
-import Shop from "./Shop.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "NavBar",
-    element: <NavBar />,
-    children: [
-      { path: "Home", element: <Home />},
-      { path: "Shop", element: <Shop />},
-      { path: "Cart", element: <Cart />},
-    ]
-  }
-])
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
