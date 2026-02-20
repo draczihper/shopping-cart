058ac1ee8ac5bffbad0b5c8a3abafc3a8724865d
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-20 23:18:27 +0300

    Add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router';
 import './App.css'
+
 
 const App = () => {
 
   return (
     <BrowserRouter>
-      
+      <Routes> 
+        <Route element={<Layout/>}> 
+          <Route path='/'/>
+        </Route>
+      </Routes>      
     </BrowserRouter>
   )
 }
