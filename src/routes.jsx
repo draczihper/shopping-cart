a54d4c0ce69b30421b0e51edf48985df9d0d36b7
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2026-02-26 22:46:59 +0300

    Add routes jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import App from "./App";
 import Shop from "./Shop";
 import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/Shop",
